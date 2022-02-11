5fdd3619853c11a324f058f2907eea2e5cb89515
tulleturkey <95871168+tulleturkey@users.noreply.github.com>
2022-02-11 22:43:49 +0800

    Link forecast data using API

--- src/app.js
+++ src/app.js
@@ -13,29 +13,40 @@ minutes = `0${minutes}`;
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun","Mon", "Tue","Wed","Thu","Fri","Sat"];
+return days[day];
+}
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+ 
  let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-  forecastHTML = forecastHTML + 
-`
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
   <div class="col-2">
     <div class="weather-forecast-date">
-    ${day}
+    ${formatDay(forecastDay.dt)}
     </div>
-<img src="https://openweathermap.org/img/wn/04n@2x.png" alt="" width="50" />
+<img src="https://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="" width="50" />
 <div class="weather-forecast-temperature">
   <span class="weather-forecast-max">
-30°
+${Math.round(forecastDay.temp.max)}°
 </span>
  <span class="weather-forecast-min">
- 24°
+ ${Math.round(forecastDay.temp.min)}°
  </span>
 </div>
   </div>
 `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
